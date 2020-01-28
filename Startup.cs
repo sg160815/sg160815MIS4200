749577ffea8553a8048f8bc75a763344e9a3902a
Glaser <sg160815@ohio.edu>
2020-01-27 22:23:42 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sg160815MIS4200.Startup))]
+namespace sg160815MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
