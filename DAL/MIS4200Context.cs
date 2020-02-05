f48e4c6a8b0960edbad2951c5d7de8b8836dede6
Samuel Glaser <sg160815@ohio.edu>
2020-02-05 11:13:40 -0500

    Implemented data migrations

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using sg160815MIS4200.Models;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace sg160815MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+            // this method is a 'constructor' and is called when a new context is created
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, sg160815MIS4200_1.Migrations.MISContext.Configuration>("DefaultConnection")
+        }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet <OrderDetails> OrderDetails { get; set; }
+
+        //add this method - it will be used later
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sg160815MIS4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string customerFirstName { get; set; }
+        public string customerLastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+        public ICollection<Orders> Orders { get; set; }
+        public string fullName { 
+            get
+            {
+                return customerLastName + ", " + customerFirstName;
+            } 
+                }
+    }
+}
