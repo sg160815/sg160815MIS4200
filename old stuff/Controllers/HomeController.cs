872ca0e833698dd66076359eb821a87b85537815
Samuel Glaser <sg160815@ohio.edu>
2020-02-16 20:41:04 -0500

    2/16/20

---
+++ old stuff/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sg160815MIS4200.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "My MIS4200 description.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Information.";
+
+            return View();
+        }
+    }
+}
